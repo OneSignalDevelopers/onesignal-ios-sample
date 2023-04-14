b59f1723fca93546dd40fbf8c03cb90ad2ecc08a
William Shepherd <williamrshepherd@gmail.com>
2023-04-14 10:21:46 -0500

    Initial Commit

---
+++ OneSignal iOS Sample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OneSignal iOS Sample
+//
+//  Created by William Shepherd on 4/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneSignal iOS Sample/OneSignal_iOS_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneSignal_iOS_SampleApp.swift
+//  OneSignal iOS Sample
+//
+//  Created by William Shepherd on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct OneSignal_iOS_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
