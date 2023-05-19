117419345a697b21e5ee52dc83184a6366cbe809
William Shepherd <williamrshepherd@gmail.com>
2023-05-19 13:23:16 -0500

    Update to latest SDK

--- OneSignal iOS Sample/OneSignal_iOS_SampleApp.swift
+++ OneSignal iOS Sample/OneSignal_iOS_SampleApp.swift
@@ -20,28 +20,13 @@ struct OneSignal_iOS_SampleApp: App {
     }
 }
 
-class AppDelegate: UIResponder, UIApplicationDelegate, OSInAppMessageLifecycleHandler {
+class AppDelegate: UIResponder, UIApplicationDelegate, OSInAppMessageLifecycleListener {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         OneSignal.Debug.setLogLevel(.LL_VERBOSE)
         OneSignal.initialize("202d4f61-1ca9-42df-9d36-bb17d8613abf", withLaunchOptions: launchOptions)
-        OneSignal.InAppMessages.setLifecycleHandler(self)
+        
+        print("Is it paused", OneSignal.InAppMessages.paused)
     
         return true
-    }
-    
-    func onWillDisplay(_ message: OSInAppMessage) {
-        print("IAM onWillDisplay \(message)")
-    }
-    
-    func onDidDisplay(_ message: OSInAppMessage) {
-        print("IAM onDidDisplay \(message)")
-    }
-    
-    func onWillDismiss(_ message: OSInAppMessage) {
-        print("IAM onWillDismiss \(message)")
-    }
-    
-    func onDidDismiss(_ message: OSInAppMessage) {
-        print("IAM onDidDismiss \(message)")
     }
 }
