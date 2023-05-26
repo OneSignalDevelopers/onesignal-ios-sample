6ded527fb35a959a8479785b2b1de9a69021efe4
William Shepherd <williamrshepherd@gmail.com>
2023-05-26 11:31:10 -0500

    Functions to start a Live Activity

--- OneSignal iOS Sample/ContentView.swift
+++ OneSignal iOS Sample/ContentView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 import ActivityKit
-import OneSignalFrameworkWrapper
+import OneSignalFramework
+import SampleWidgetExtensionExtension
 
 struct ContentView: View {
+    @State private var activity: Activity<SampleWidgetExtensionAttributes>? = nil
+    
+    let activityId = "Some_LA_ID"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -59,28 +64,19 @@ struct ContentView: View {
             }
             
             Button(action: {
-                let attributes =  SampleWidgetExtensionAttributes(numberOfPizzas: 4, totalAmount: "$45.43", orderNumber: "420")
-                let contentState = SampleWidgetExtensionAttributes.ContentState(driverName: "William", deliveryTimer: Date.now...Date())
-                let activityContent = ActivityContent(state: contentState, staleDate: Calendar.current.date(byAdding: .minute, value: 30, to: Date())!)
-
-                do {
-                    let activity = try Activity<SampleWidgetExtensionAttributes>.request(
-                         attributes: attributes,
-                         content: activityContent,
-                         pushType: .token)
-
-                     Task {
-                         for await data in activity.pushTokenUpdates {
-                             let myToken = data.map {String(format: "%02x", $0)}.joined()
-                             OneSignal.LiveActivities.enter("activityId", withToken: "token")
-                         }
-                     }
-                 } catch (let error) {
-                     print(error.localizedDescription)
-                 }
-                
+               startActivity()
             }) {
                 Text("Start Live Activity")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Button(action: {
+               stopActivity()
+            }) {
+                Text("End Live Activity")
                     .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
@@ -89,6 +85,34 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    func startActivity() {
+        let attributes =  SampleWidgetExtensionAttributes(numberOfPizzas: 4, totalAmount: "$45.43", orderNumber: "420")
+        let contentState = SampleWidgetExtensionAttributes.ContentState(driverName: "William", deliveryTimer: Date.now...Date())
+        let activityContent = ActivityContent(state: contentState, staleDate: Calendar.current.date(byAdding: .minute, value: 30, to: Date())!)
+
+        do {
+            activity = try Activity<SampleWidgetExtensionAttributes>.request(
+                 attributes: attributes,
+                 content: activityContent,
+                 pushType: .token)
+            
+            Task {
+                for await data in activity!.pushTokenUpdates {
+                    let token = data.map {String(format: "%02x", $0)}.joined()
+                    print("Live Activity Push Token: ", token)
+                    OneSignal.LiveActivities.enter(activityId, withToken: token)
+                }
+            }
+         } catch (let error) {
+             print(error.localizedDescription)
+         }
+    }
+    
+    func stopActivity() {
+        OneSignal.LiveActivities.exit(activityId)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
