92fd6898d8f82b069341e5c0a448f1efb7eb98a5
William Shepherd <williamrshepherd@gmail.com>
2024-06-14 16:04:03 -0500

    Add listener for pushToStart tokens

--- OneSignal iOS Sample/OneSignal_iOS_SampleApp.swift
+++ OneSignal iOS Sample/OneSignal_iOS_SampleApp.swift
@@ -24,6 +24,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, OSInAppMessageLifecycleLi
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         OneSignal.Debug.setLogLevel(.LL_VERBOSE)
         OneSignal.initialize("202d4f61-1ca9-42df-9d36-bb17d8613abf", withLaunchOptions: launchOptions)
+        
+        if #available(iOS 17.2, *) {
+            Task {
+                for try await data in Activity<LiveActivityAttributes>.pushToStartTokenUpdates {
+                    let token = data.map {String(format: "%02x", $0)}.joined()
+                    OneSignal.LiveActivities.setPushToStartToken(LiveActivityAttributes.self, withToken: token)
+                }
+            }
+        }
     
         return true
     }
